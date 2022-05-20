c62b0e49e47563861a68ecbee611ce92593115e6
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-05-19 18:19:59 -0700

    npm i react-router-dom, setup/tested routesdeleted test.js file

--- src/pages/Studio.jsx
+++ src/pages/Studio.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Home() {
+function Studio() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Studio</h1>
     </div>
   )
 }
 
-export default Home
+export default Studio
