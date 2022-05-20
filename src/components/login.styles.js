1169e55398bafd91efe193a80a5fce25cfe3da4f
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-05-20 15:23:52 -0700

    created signup page

--- src/components/login.styles.js
+++ src/components/login.styles.js
@@ -12,3 +12,16 @@ export const Background = styled.video`
   display: block;
   margin: 0 auto;
 `;
+
+export const LoginImg = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 50%;
+  left: 50%;
+  object-fit: cover;
+  transform: translate(-50%, -50%);
+  z-index: -2;
+  display: block;
+  margin: 0 auto;
+`;
