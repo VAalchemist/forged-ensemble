24651473adb2e73d05788c45116a932bfc0880ab
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-22 08:45:24 -0700

    grid box sizing

--- src/pages/Studio.jsx
+++ src/pages/Studio.jsx
@@ -11,14 +11,14 @@ function Studio() {
       </Background>
       {/* soundboard container */}
       <div className='max-w-screen-lg p-4 mx-auto'>
-        <div className='grid grid-cols-4 md:grid-cols-3 gap-4 mt-12'>
+        <div className='grid grid-cols-4 md:grid-cols-3 gap-4 mt-28'>
           
 
-        <div data-note='C' class="w-full h-full shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#00dfff]">C</div>
-    <div data-note='Db' class="w-full h-28 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#ff8f00]">Db</div>
-    <div data-note='D' class="w-full h-28 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#FC77EB]">D</div>
-    <div data-note='E' class="w-full h-28 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#36EB42]">E</div>
-    <div data-note='Eb' class="w-full h-28 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#00dfff]">Eb</div>
+        <div data-note='C' class=" w-28 md:w-full h-auto shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#00dfff]">C</div>
+    <div data-note='Db' class="w-full h-28 md:h-64 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#ff8f00]">Db</div>
+    <div data-note='D' class="w-full lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#FC77EB]">D</div>
+    <div data-note='E' class="w-full sm:h-36 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#36EB42]">E</div>
+    <div data-note='Eb' class="w-28 md:w-full h-28 sm:h-36 md:h-64 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#00dfff]">Eb</div>
     <div data-note='F' class="w-full h-28 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#ff8f00]">F</div>
     <div data-note='Gb' class="w-full h-28 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#FC77EB]">Gb</div>
     <div data-note='G' class="w-full h-28 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#36EB42]">G</div>
@@ -29,7 +29,7 @@ function Studio() {
           
           
       </div>
-      </div>      
+      </div> 
     </>
   )
 }
