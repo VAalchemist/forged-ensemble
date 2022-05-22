fb7815cbd9683e483fbed28075298910d6b49b02
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-21 23:59:58 -0700

    centered container for soundboard

--- src/pages/Studio.jsx
+++ src/pages/Studio.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import music from "../images/home.mp4";
+import { Background } from "../components/login.styles";
 
 function Studio() {
   return (
-    <header>
-
-      <div>
+    <header className=''>
+      <Background autoPlay loop muted>
+        <source src={music} type="video/mp4" />
+      </Background>
+      <div className='flex justify-center items-center h-screen'>
+        <div className='bg-white w-[500px] h-[500px]'>
         <div data-note='C' className='bg-[#00dfff]'></div>
         <div data-note='Db' className='bg-[#00dfff]'></div>
         <div data-note='D' className='bg-[#00dfff]'></div>
@@ -19,7 +24,7 @@ function Studio() {
         <div data-note='Bb' className='bg-[#36EB42]'></div>
         <div data-note='B' className='bg-[#36EB42]'></div>
       </div>
-
+      </div>      
     </header>
   )
 }
