6b70c3420e1ae3519443cc9c6a809415211085ba
EddieLo <eddarlostudy@yahoo.com>
2022-05-24 06:03:47 -0700

    removed extra files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
 import Nav from './Nav'
 import { Feed } from '../components/home.styles'
+import { CgMusicSpeaker, CgMusic } from 'react-icons/cg';
 
 
 function Home() {
   return (
     <>
     <Nav />
-    <div className='absolute text-white w-full h-screen flex justify-center items-center p-4'>
-      <Feed>Feed Coming Soon</Feed>
+    <div className='abosulte h-[15rem] text-white w-full flex justify-center items-center p-2'>
+      <CgMusic size={15} />
+      <CgMusicSpeaker size={40} />
+      <Feed>Feed Coming Soon </Feed>
+      <CgMusicSpeaker size={40} />
+      <CgMusic size={15} />
+    <div className='absolute w-[50%] h-14 bg-[#00bdff] z-[-1] opacity-[30%] rounded-[15px]'></div>
+    {/* <div className='absolute w-80 h-14 bg-[#70ED66] skew-x-[30deg] z-[-1] mr-[12.5rem] opacity-[45%]'></div> */}
+    {/* <div className='absolute w-[50px] h-12 bg-[#E040CA] skew-x-[30deg] z-[-1] ml-[15rem] opacity-[45%]'></div> */}
+    {/* <div className='absolute w-[50px] h-12 bg-blue-300 skew-x-[30deg] z-[-1] ml-[25rem] opacity-[45%]'></div> */}
+    {/* <div className='absolute w-[50px] h-12 bg-[#32C8E4] skew-x-[30deg] z-[-1] ml-[35rem] opacity-[45%]'></div> */}
     </div>
+  
       
     </>
   );
