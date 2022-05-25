e6d1b2135f1c06f967766bab265e00e9de97fbf7
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-24 23:51:45 -0700

    swtich each pad to styled component

--- client/src/components/studio.styles.js
+++ client/src/components/studio.styles.js
@@ -1,7 +1,54 @@
 import tw from 'tailwind-styled-components';
 
 
-export const BeatPad = tw.div`
+export const BlueBeatPad = tw.div`
+bg-[#00dfff]
+w-28
+sm:w-40
+md:w-full
+h-28
+sm:h-36
+md:h-60
+lg:h-72
+shadow-2xl
+shadow-orange-100
+pb-full
+rounded-xl
+`;
+
+export const OrangeBeatPad = tw.div`
+bg-[#ff8f00]
+w-28
+sm:w-40
+md:w-full
+h-28
+sm:h-36
+md:h-60
+lg:h-72
+shadow-2xl
+shadow-orange-100
+pb-full
+rounded-xl
+`;
+
+
+export const PinkBeatPad = tw.div`
+bg-[#FC77EB]
+w-28
+sm:w-40
+md:w-full
+h-28
+sm:h-36
+md:h-60
+lg:h-72
+shadow-2xl
+shadow-orange-100
+pb-full
+rounded-xl
+`;
+
+export const GreenBeatPad = tw.div`
+bg-[#36EB42]
 w-28
 sm:w-40
 md:w-full
--- client/src/pages/Studio.jsx
+++ client/src/pages/Studio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import music from "../images/home.mp4";
 import { Background } from "../components/login.styles";
-import { BeatPad } from '../components/studio.styles';
+import { BlueBeatPad, PinkBeatPad, GreenBeatPad, OrangeBeatPad } from '../components/studio.styles';
 
 
 function Studio() {
@@ -13,18 +13,18 @@ function Studio() {
           <source src={music} type="video/mp4" />
         </Background>
         
-        <BeatPad data-note='C' className="bg-[#00dfff]">C</BeatPad>
-          <div data-note='Db' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#ff8f00]">Db</div>
-          <div data-note='D' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#FC77EB]">D</div>
-          <div data-note='Eb' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#36EB42]">E</div>
-          <div data-note='E' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#00dfff]">Eb</div>
-          <div data-note='F' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#ff8f00]">F</div>
-          <div data-note='Gb' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#FC77EB]">Gb</div>
-          <div data-note='G' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#36EB42]">G</div>
-          <div data-note='Ab' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#00dfff]">Ab</div>
-          <div data-note='A' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#ff8f00]">A</div>
-          <div data-note='Bb' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#FC77EB]">Bb</div>
-          <div data-note='B' className=" w-28 sm:w-40 md:w-full h-28 sm:h-36 md:h-60 lg:h-72 shadow-2xl shadow-orange-100 pb-full rounded-xl bg-[#36EB42]">B</div>
+        <BlueBeatPad data-note='C'>C</BlueBeatPad>
+        <OrangeBeatPad data-note='Db'>Db</OrangeBeatPad>
+        <PinkBeatPad data-note='D'>D</PinkBeatPad>
+        <GreenBeatPad data-note='Eb'>E</GreenBeatPad>
+        <BlueBeatPad data-note='E'>Eb</BlueBeatPad>
+        <OrangeBeatPad data-note='F'>F</OrangeBeatPad>
+        <PinkBeatPad data-note='Gb'>Gb</PinkBeatPad>
+        <GreenBeatPad data-note='G'>G</GreenBeatPad>
+        <BlueBeatPad data-note='Ab'>Ab</BlueBeatPad>
+        <OrangeBeatPad data-note='A'>A</OrangeBeatPad>
+        <PinkBeatPad data-note='Bb'>Bb</PinkBeatPad>
+        <GreenBeatPad data-note='B'>B</GreenBeatPad>
 
       </div>
   </div>
