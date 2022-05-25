3a24cf15188083dda1e10ee7e9cae97a36b0ba0f
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-25 03:07:03 -0700

    render buttons through mapping

--- client/src/components/studio.styles.js
+++ client/src/components/studio.styles.js
@@ -1,7 +1,7 @@
 import tw from 'tailwind-styled-components'; 
 
 
-export const BeatPad = tw.div`
+export const BeatPad = tw.button`
 w-28
 sm:w-40
 md:w-full
@@ -13,19 +13,19 @@ shadow-2xl
 shadow-orange-100
 pb-full
 rounded-xl
+hover:ring-8
 hover:duration-500
+active:duration-500
 `;
 
 export const BlueBeatPad = tw(BeatPad)`
 bg-[#00dfff]
-hover:ring-8
 hover:ring-cyan-600
 hover:bg-[#9ae7f2]
 `;
 
 export const OrangeBeatPad = tw(BeatPad)`
 bg-[#ff8f00]
-hover:ring-8
 hover:ring-orange-500
 hover:bg-[#ebc694]
 `;
@@ -33,14 +33,12 @@ hover:bg-[#ebc694]
 
 export const PinkBeatPad = tw(BeatPad)`
 bg-[#FC77EB]
-hover:ring-8
 hover:ring-pink-500
 hover:bg-[#f5a2ea]
 `;
 
 export const GreenBeatPad = tw(BeatPad)`
 bg-[#36EB42]
-hover:ring-8
 hover:ring-green-600
 hover:bg-[#84f48b]
 `;
--- client/src/pages/Studio.jsx
+++ client/src/pages/Studio.jsx
@@ -1,40 +1,67 @@
-import React from 'react';
-import music from "../images/home.mp4";
-import { Background } from "../components/login.styles";
-import {
-  BlueBeatPad,
-  PinkBeatPad,
-  GreenBeatPad,
-  OrangeBeatPad
-} from '../components/studio.styles';
+import React, { Component } from 'react';
+import { Howl, Howler } from 'howler';
+import { BeatPad } from '../components/studio.styles';
+// import music from "../images/home.mp4";
+// import { Background } from "../components/login.styles";
+
+// import {
+//   BlueBeatPad,
+//   PinkBeatPad,
+//   GreenBeatPad,
+//   OrangeBeatPad
+// } from '../components/studio.styles';
+import C from '../notes/C.mp3';
+import Db from '../notes/Db.mp3';
+import D from '../notes/D.mp3';
+import Eb from '../notes/Eb.mp3';
+import E from '..//notes/E.mp3';
+import F from '..//notes/F.mp3';
+import Gb from '../notes/Gb.mp3';
+import G from '../notes/G.mp3';
+import Ab from '../notes/Ab.mp3';
+import A from '../notes/A.mp3';
+import Bb from '../notes/Bb.mp3';
+import B from '../notes/B.mp3';
+
+const notes = [
+  { sound: C, label: 'C' },
+  { sound: Db, label: 'Db' },
+  { sound: D, label: 'D' },
+  { sound: Eb, label: 'Eb'},
+  { sound: E, label: 'E' },
+  { sound: F, label: 'F' },
+  { sound: Gb, label: 'Gb'},
+  { sound: G, label: 'G' },
+  { sound:Ab, label: 'Ab'},
+  { sound: A, label: 'A'},
+  { sound: Bb, label: 'Bb'},
+  { sound: B, label: 'B'}  
+];
 
 
-function Studio() {
-  return (
+class Studio extends Component {
+  SoundPlay = (src) => {
+    const sound = new Howl({ src })
+    
+    sound.play();
+  };
 
-    <div className=' max-w-screen-lg p-4 mx-auto mt-20 lg:mt-2'>
-      <div className='grid grid-cols-4 md:grid-cols-3 gap-4 '>
-        <Background autoPlay loop muted  >
-          <source src={music} type="video/mp4" />
-        </Background>
-        
-        <BlueBeatPad data-note='C'>C</BlueBeatPad>
-        <OrangeBeatPad data-note='Db'>Db</OrangeBeatPad>
-        <PinkBeatPad data-note='D'>D</PinkBeatPad>
-        <GreenBeatPad data-note='Eb'>E</GreenBeatPad>
-        <BlueBeatPad data-note='E'>Eb</BlueBeatPad>
-        <OrangeBeatPad data-note='F'>F</OrangeBeatPad>
-        <PinkBeatPad data-note='Gb'>Gb</PinkBeatPad>
-        <GreenBeatPad data-note='G'>G</GreenBeatPad>
-        <BlueBeatPad data-note='Ab'>Ab</BlueBeatPad>
-        <OrangeBeatPad data-note='A'>A</OrangeBeatPad>
-        <PinkBeatPad data-note='Bb'>Bb</PinkBeatPad>
-        <GreenBeatPad data-note='B'>B</GreenBeatPad>
+  RenderNote = () => {
+    return notes.map((noteObj, index) => {
+      return (
+        <BeatPad key={index} onClick={()=> this.SoundPlay(noteObj.sound)}>
+          {noteObj.label}
+        </BeatPad>
+      );
+    })
+  }
 
-      </div>
-  </div>
-
-  )
+  render() {
+    Howler.volume(1.0);
+    return <div>
+    {this.RenderNote()}
+    </div>;
+  }
 }
 
-export default Studio
+export default Studio;
