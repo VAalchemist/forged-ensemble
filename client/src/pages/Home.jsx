f9258e1051f4e5286466e2475f9c397eb8a42e80
EddieLo <eddarlostudy@yahoo.com>
2022-05-25 23:48:42 -0700

    updated demo page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,11 +16,11 @@ function Home() {
       <Feed>Feed Coming Soon </Feed>
       <CgMusicSpeaker size={40} />
       <CgMusic size={15} />
-    <div className='absolute w-[50%] h-14 bg-[#00bdff] z-[-1] opacity-[30%] rounded-[15px]'></div>
+    {/* <div className='absolute w-[50%] h-14 bg-[#00bdff] z-[-1] opacity-[30%] rounded-[15px]'></div> */}
     {/* <div className='absolute w-80 h-14 bg-[#70ED66] skew-x-[30deg] z-[-1] mr-[12.5rem] opacity-[45%]'></div> */}
-    {/* <div className='absolute w-[50px] h-12 bg-[#E040CA] skew-x-[30deg] z-[-1] ml-[15rem] opacity-[45%]'></div> */}
-    {/* <div className='absolute w-[50px] h-12 bg-blue-300 skew-x-[30deg] z-[-1] ml-[25rem] opacity-[45%]'></div> */}
-    {/* <div className='absolute w-[50px] h-12 bg-[#32C8E4] skew-x-[30deg] z-[-1] ml-[35rem] opacity-[45%]'></div> */}
+    {/* <div className='absolute w-[50px] h-12 bg-[#E040CA] skew-x-[30deg] z-[-1] ml-[15rem] opacity-[45%]'></div>
+    <div className='absolute w-[50px] h-12 bg-blue-300 skew-x-[30deg] z-[-1] ml-[25rem] opacity-[45%]'></div>
+    <div className='absolute w-[50px] h-12 bg-[#32C8E4] skew-x-[30deg] z-[-1] ml-[35rem] opacity-[45%]'></div> */}
     </div>
   
  
