9cacbe29cce935bb423abdf8bd6e0a3e69ccb52e
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-25 19:37:37 -0700

    before code refactoring for sound implementation

--- client/src/pages/Studio.jsx
+++ client/src/pages/Studio.jsx
@@ -1,15 +1,12 @@
 import { Howl, Howler } from 'howler';
-import {BeatPad} from '../components/studio.styles';
-import { Component } from 'react';
-import music from "../images/home.mp4";
-import { Background } from "../components/login.styles";
+// import music from "../images/home.mp4";
+// import { Background } from "../components/login.styles";
 import {
   BlueBeatPad,
   PinkBeatPad,
   GreenBeatPad,
   OrangeBeatPad
 } from '../components/studio.styles';
-
 
 import Q from '../drum kit/bewp.wav';
 import W from '../drum kit/boom.wav';
@@ -25,88 +22,57 @@ import C from '../drum kit/shortkick.wav';
 import V from '../drum kit/shortshake.wav';
 
 
-const notes = [
-  { sound: Q, label: 'C' },
-  { sound: W, label: 'Db' },
-  { sound: E, label: 'D' },
-  { sound: R, label: 'Eb'},
-  { sound: A, label: 'E' },
-  { sound: S, label: 'F' },
-  { sound: D, label: 'Gb'},
-  { sound: F, label: 'G' },
-  { sound: Z, label: 'Ab'},
-  { sound: X, label: 'A'},
-  { sound: C, label: 'Bb'},
-  { sound: V, label: 'B'}  
-];
-
-
-
 
 
 function Studio() {
+
+  const q = (Q);
+  const w = (W);
+  const e = (E);
+  const r = (R);
+  const a = (A);
+  const s = (S);
+  const d = (D);
+  const f = (F);
+  const z = (Z);
+  const x = (X);
+  const c = (C);
+  const v = (V);
   
-  SoundPlay = (src) => {
-    const sound = new Howl({ src })
-    
-    sound.play(notes);
+
+  const Beats = (src) => {
+    const sound = new Howl({
+      src,
+      html5: true,
+    });
+    sound.play();
   };
 
-  const [beat, setBeat] = useState('');
-
-  const handleClick = (event) => {
-    
-  };
-
+  Howler.volume(0.5);
 
   return (
     <div className=' max-w-screen-lg p-4 mx-auto mt-20 lg:mt-2'>
       <div className='grid grid-cols-4 md:grid-cols-3 gap-4 '>
-        <Background autoPlay loop muted  >
+        {/* <Background autoPlay loop muted  >
           <source src={music} type="video/mp4" />
         </Background>
+         */}
         
-        
-        <BlueBeatPad id='0' onClick={handleClick} data-note='Q'>Q</BlueBeatPad>
-        <OrangeBeatPad  data-note='W'>W</OrangeBeatPad>
-        <PinkBeatPad data-note='E'>E</PinkBeatPad>
-        <GreenBeatPad data-note='R'>R</GreenBeatPad>
-        <BlueBeatPad data-note='A'>A</BlueBeatPad>
-        <OrangeBeatPad data-note='S'>S</OrangeBeatPad>
-        <PinkBeatPad data-note='D'>D</PinkBeatPad>
-        <GreenBeatPad data-note='F'>F</GreenBeatPad>
-        <BlueBeatPad data-note='Z'>Z</BlueBeatPad>
-        <OrangeBeatPad data-note='X'>X</OrangeBeatPad>
-        <PinkBeatPad data-note='C'>C</PinkBeatPad>
-        <GreenBeatPad data-note='V'>V</GreenBeatPad>
+        <BlueBeatPad onClick={() => Beats(q)} data-note='Q'>Q</BlueBeatPad>
+        <OrangeBeatPad id='1'  data-note='W'>W</OrangeBeatPad>
+        <PinkBeatPad id='2' data-note='E'>E</PinkBeatPad>
+        <GreenBeatPad id='3' data-note='R'>R</GreenBeatPad>
+        <BlueBeatPad id='4' data-note='A'>A</BlueBeatPad>
+        <OrangeBeatPad id='5' data-note='S'>S</OrangeBeatPad>
+        <PinkBeatPad id='6' data-note='D'>D</PinkBeatPad>
+        <GreenBeatPad id='7' data-note='F'>F</GreenBeatPad>
+        <BlueBeatPad id='8' data-note='Z'>Z</BlueBeatPad>
+        <OrangeBeatPad id='9' data-note='X'>X</OrangeBeatPad>
+        <PinkBeatPad id='10' data-note='C'>C</PinkBeatPad>
+        <GreenBeatPad id='11' data-note='V'>V</GreenBeatPad>
 
       </div>
   </div>
-  )
+)}
 
-}
-
-
-
-// class Studio extends Component {
- 
-
-//   RenderNote = () => {
-//     return notes.map((noteObj, index) => {
-//       return (
-//         <BeatPad key={index} onClick={()=> this.SoundPlay(noteObj.sound)}>
-//           {noteObj.label}
-//         </BeatPad>
-//       );
-//     })
-//   }
-
-//   render() {
-//     Howler.volume(1.0);
-//     return <div>
-
-//         	{this.RenderNote()}
-// </div>;
-//   }
-// }
 export default Studio;
