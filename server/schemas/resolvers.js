d0902faefe82c55820b95585b389393fbec13312
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-28 22:03:40 -0700

    corrected href pathing in login.jsx

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Beats} = require('../models');
-const { User, Files } = require('../models');
+const { User, Beats, Files} = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
